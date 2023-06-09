364be116069d1cbc035789c379d2594ee63cbb99
Maciej Jędrzejczak <mjedrzejczak@edu.cdv.pl>
2023-06-10 00:29:32 +0200

    Addition of CameraShake with DOTween, cleaning old scripts

--- Assets/Scripts/Level/AmIPickedUp.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public class AmIPickedUp : MonoBehaviour
-{
-    [SerializeField] ScriptableObjectbool amIPickedUp;
-}
